063041a8186b9ff6b627f18e7fe8729423e280ef
anasAbounouar <anas.2002.abounouar@gmail.com>
2023-11-13 22:02:39 +0100

    integrated a page loader

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
-    <NavBar />
+    <NavBar  v-if="isloaded"  />
+    <PageLoader v-if="!isloaded" />
     <RouterView />
   </div>
 </template>
@@ -8,10 +9,22 @@
 <script setup>
 import NavBar from "@/components/NavBar.vue";
 import { RouterView } from "vue-router";
+import PageLoader from "./components/PageLoader.vue";
+import { onMounted,ref } from "vue";
+const isloaded = ref(false);
+
+// Use the 'onMounted' lifecycle hook to set 'isloaded' to true
+onMounted(() => {
+  document.onreadystatechange = () => {
+    if (document.readyState === "complete") {
+      // Set isloaded to true when the document is fully loaded
+      isloaded.value = true;
+    }
+  };
+});
 </script>
 <style lang="scss">
 html {
   scroll-behavior: smooth;
 }
-  
 </style>
